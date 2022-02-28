7e89d34286bcb12b446eb33ed2fff81d0ec10499
omkar-wohlig <omkarwohlig@gmail.com>
2022-02-28 14:53:08 +0530

    initial

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar center-collapsed v-model="active">
+      <template #left>
+        <img class="" src="../../public/store.svg" height="64" alt="" />
+      </template>
+      <vs-navbar-item :active="active == 'guide'" id="guide">
+        Home
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'docs'" id="docs">
+        Order
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'components'" id="components">
+        Services
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'license'" id="license">
+        Contact
+      </vs-navbar-item>
+      <template #right>
+        <vs-button flat>Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+  <script>
+export default {
+  data: () => ({
+    active: "guide",
+  }),
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,22 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Vuesax from "vuesax";
+import veu from "./components/veu.vue";
+
+import "vuesax/dist/vuesax.css"; //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+});
+
+Vue.component("veu", veu);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
